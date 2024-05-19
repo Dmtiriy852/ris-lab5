1a1b56757388232233b120e912215350d3ef3900
Dmitriy <dima.852.dima@gmail.com>
2024-05-19 19:50:11 +0300

    user.model.js

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,17 +1,9 @@
-import { v4 as uuidv4 } from 'uuid';
-
-class User {
-  constructor({ id = uuidv4(), name = 'USER', login = 'user', password = 'P@55w0rd' } = {}) {
+class Abiturient {
+  constructor(id, name, age) {
     this.id = id;
     this.name = name;
-    this.login = login;
-    this.password = password;
-  }
-
-  static toResponse(user) {
-    const { id, name, login } = user;
-    return { id, name, login };
+    this.age = age;
   }
 }
 
-export default User;
+module.exports = Abiturient;
